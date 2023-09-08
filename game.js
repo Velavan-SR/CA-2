50a78bee2da4ebbebff987b23ec5aab6f48a8a84
Velavan Sundharamoorthy <velavan.sundharamoorth@kalvium.community>
2023-09-08 14:29:32 +0530

    Done

--- game.js
+++ game.js
@@ -24,6 +24,11 @@
         return Math.floor(Math.random()*2)
     }
 
+       // Easy Gameplay
+
+
+if(window.localStorage.getItem('id')=='easy'){
+
     document.addEventListener("keydown", function(event) {
         if (event.key === ' ') {
           jump();
@@ -60,9 +65,9 @@
 
     intervalId = setInterval(function () {
                     if (blockState) {
-                    setTimeout(block,2500);
+                    setTimeout(block,3000);
                     }
-                },2500);
+                },3000);
 
     function jump(){
         if (document.getElementById('sasuke').classList != 'animate'){
@@ -85,7 +90,7 @@
             image.className = "block";
             game.appendChild(image);
             
-            setTimeout(removeBlock, 2500);
+            setTimeout(removeBlock, 3000);
             
             blockState = true;
             
@@ -109,7 +114,7 @@
         score++;
         scoree.innerText=score;
     }
-
+};
 
 
     // FUNCTIONALITIES FOR CHECKING IF THE ELEMENTS ARE COLLIDING
@@ -117,6 +122,236 @@
     function checkDead() {
         const mainCharacter = document.getElementById('sasuke').getBoundingClientRect();
         const block = document.querySelector('.block');
+    
+        if (block) {
+            const blockRect = block.getBoundingClientRect();
+            
+            if (window.innerHeight - mainCharacter.bottom < 250 && blockRect.left <= 225 && blockRect.left >=105) {
+                gameStopped = true;
+                blockState = false;
+                scoreState = false;
+                backgroundMusic.pause();
+                window.localStorage.setItem('score',score);
+                alert('Oops! YOU LOST');
+                window.location.href='result.html';
+            }
+        }
+    }
+
+
+
+        // Mid Gameplay
+
+if(window.localStorage.getItem('id')=='mid'){
+
+    document.addEventListener("keydown", function(event) {
+        if (event.key === ' ') {
+          jump();
+          jumpEffect.currentTime = 0;
+          jumpEffect.play();
+        }
+      });
+
+
+    document.body.addEventListener('click',() =>{
+        jump();
+        jumpEffect.currentTime = 0;
+        jumpEffect.play();
+
+        if (!bgmState){
+            backgroundMusic = new Audio('./assessts/Naruto-ost.mp3');
+            backgroundMusic.play();
+            backgroundMusic.loop = true;
+            bgmState = true;
+        }
+
+        document.getElementById('instructions').style.visibility='hidden';
+
+        setInterval(checkDead,10);
+        
+        if (!removeState) {
+            kakashi.remove();
+            naruto.remove();
+            removeState=true;
+        };
+        if (!blockState) {block()};
+        if (!scoreState) {setScore()};
+    });
+
+    intervalId = setInterval(function () {
+                    if (blockState) {
+                    setTimeout(block,2000);
+                    }
+                },2000);
+
+    function jump(){
+        if (document.getElementById('sasuke').classList != 'animate'){
+            document.getElementById('sasuke').classList.add("animate");
+            setTimeout(removeJump,1250);
+        }
+    };
+
+    function removeJump(){
+        document.getElementById('sasuke').classList.remove('animate');
+    };
+
+    function block() {
+        if (gameStopped) {
+            clearInterval(intervalId);
+        } else {
+            const image = document.createElement("img");
+            image.src = c % 2 === 0 ? "assessts/block0.gif" : "assessts/block1.gif";
+            image.id = "h";
+            image.className = "block-mid";
+            game.appendChild(image);
+            
+            setTimeout(removeBlock, 2000);
+            
+            blockState = true;
+            
+            c++;
+        }
+    }
+    
+
+    function removeBlock(){
+        document.getElementById('h').remove()
+    };
+
+    setInterval(function () {
+        if (scoreState) {
+            setScore();
+        }
+    },1000);
+
+    function setScore(){
+        scoreState = true;
+        score++;
+        scoree.innerText=score;
+    }
+};
+
+
+    // FUNCTIONALITIES FOR CHECKING IF THE ELEMENTS ARE COLLIDING
+
+    function checkDead() {
+        const mainCharacter = document.getElementById('sasuke').getBoundingClientRect();
+        const block = document.querySelector('.block-mid');
+    
+        if (block) {
+            const blockRect = block.getBoundingClientRect();
+            
+            if (window.innerHeight - mainCharacter.bottom < 250 && blockRect.left <= 225 && blockRect.left >=105) {
+                gameStopped = true;
+                blockState = false;
+                scoreState = false;
+                backgroundMusic.pause();
+                window.localStorage.setItem('score',score);
+                alert('Oops! YOU LOST');
+                window.location.href='result.html';
+            }
+        }
+    }
+
+
+        //Hard Gameplay
+
+
+if(window.localStorage.getItem('id')=='hard'){
+
+    document.addEventListener("keydown", function(event) {
+        if (event.key === ' ') {
+          jump();
+          jumpEffect.currentTime = 0;
+          jumpEffect.play();
+        }
+      });
+
+
+    document.body.addEventListener('click',() =>{
+        jump();
+        jumpEffect.currentTime = 0;
+        jumpEffect.play();
+
+        if (!bgmState){
+            backgroundMusic = new Audio('./assessts/Naruto-ost.mp3');
+            backgroundMusic.play();
+            backgroundMusic.loop = true;
+            bgmState = true;
+        }
+
+        document.getElementById('instructions').style.visibility='hidden';
+
+        setInterval(checkDead,10);
+        
+        if (!removeState) {
+            kakashi.remove();
+            naruto.remove();
+            removeState=true;
+        };
+        if (!blockState) {block()};
+        if (!scoreState) {setScore()};
+    });
+
+    intervalId = setInterval(function () {
+                    if (blockState) {
+                    setTimeout(block,1500);
+                    }
+                },1500);
+
+    function jump(){
+        if (document.getElementById('sasuke').classList != 'animate-hard'){
+            document.getElementById('sasuke').classList.add("animate-hard");
+            setTimeout(removeJump,600);
+        }
+    };
+
+    function removeJump(){
+        document.getElementById('sasuke').classList.remove('animate-hard');
+    };
+
+    function block() {
+        if (gameStopped) {
+            clearInterval(intervalId);
+        } else {
+            const image = document.createElement("img");
+            image.src = c % 2 === 0 ? "assessts/block0.gif" : "assessts/block1.gif";
+            image.id = "h";
+            image.className = "block-hard";
+            game.appendChild(image);
+            
+            setTimeout(removeBlock, 1500);
+            
+            blockState = true;
+            
+            c++;
+        }
+    }
+    
+
+    function removeBlock(){
+        document.getElementById('h').remove()
+    };
+
+    setInterval(function () {
+        if (scoreState) {
+            setScore();
+        }
+    },1000);
+
+    function setScore(){
+        scoreState = true;
+        score++;
+        scoree.innerText=score;
+    }
+};
+
+
+    // FUNCTIONALITIES FOR CHECKING IF THE ELEMENTS ARE COLLIDING
+
+    function checkDead() {
+        const mainCharacter = document.getElementById('sasuke').getBoundingClientRect();
+        const block = document.querySelector('.block-hard');
     
         if (block) {
             const blockRect = block.getBoundingClientRect();
